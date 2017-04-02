7760046d326fc32c21dbe8f83771a4cab50e8557
ahmedtarek2376 <ahmed.tarek.2376@gmail.com>
2017-04-02 22:43:21 +0200

    try

--- app/controllers/reviewController.js
+++ app/controllers/reviewController.js
@@ -1,7 +1,36 @@
 let Review = require('../models/Review');
 
+//try
+
 let reviewController={
 //Write here the functions in the format of function_name:function(params)
+  
+          viewBusinessReviews: function(req, res){
+
+        var currentBusiness = req.session.currentBusiness;
+
+        var conditions = { business_id: currentBusiness };
+
+        var reviews = Review.find(conditions, function(err, reviews){
+
+			if(err){
+
+				res.send(err);
+
+                console.log(err);
+
+			}else{
+
+                console.log(reviews);
+
+                return reviews;
+
+            }
+
+		});
+
+
+    }
 };
 
 module.exports = reviewController;
--- app/models/BusinessOwner.js
+++ app/models/BusinessOwner.js
@@ -4,7 +4,8 @@ var businessownerSchema = mongoose.Schema({
 	user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
 	name: String,
 	description: String,
-	locations: [String]
+	locations: [String],
+	rating: Number,
 	});
 
 var BusinessOwner = mongoose.model("BusinessOwner", businessownerSchema);
