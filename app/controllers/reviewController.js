d0113ecdb653f59db2499d2bfe61ceef5195d1cb
shorim <mostafa_shorim@hotmail.com>
2017-04-06 18:19:14 +0200

    FInal

---
+++ app/controllers/reviewController.js
@@ -0,0 +1,154 @@
+let Review = require('../models/Review');
+var User = require('../models/User');
+let Client = require('../models/Client');
+var ObjectId = require('mongodb').ObjectID;
+var BusinessOwner = require('../models/BusinessOwner');
+
+      
+let reviewController={
+
+	
+	//clients write review about business
+	//this function receives requests for creating new reviews, and makes an entry to the database
+	//the function ensures that the request includes the required fields (user_id, business_id, comment)
+	newReview:function(req,res){
+		var review = new Review();
+		review.user_id = req.body.user_id;
+		review.business_id = req.body.business_id;
+		review.comment = req.body.comment;
+		var missingFields = req.body.user_id==null || req.body.user_id=='' || req.body.business_id==null || req.body.business_id=='' || 
+		                    req.body.comment==null || req.body.comment=='';
+
+		if(missingFields){
+			res.send('The fields: (user_id, business_id, comment) are required!');
+		}
+		else{
+			review.save(function(err){
+				if(err){
+					res.send(err);
+				}
+				else{
+					res.send('Review has been posted successfully.');
+				}
+			});
+
+		}
+	},
+
+	//by using angular's ng-show, it will be guaranteed that each user can only update his own reviews
+	//this function receives requests for editing reviews, an review has multiple attributes;
+	//the function ensures that the request includes the required fields (user_id, business_id, comment)
+	//by using angular's ng-show, it will be guaranteed that each user can only update his own reviews
+	editReview: function(req,res){
+		var review_user_id = req.body.user_id;
+		var review_newComment = req.body.comment;
+		var review_id =  req.params.id;
+		var missingFields = review_user_id==null || review_user_id=='' || review_newComment==null || review_newComment=='';
+		if(missingFields){
+			res.send('The fields: (user_id, comment) are required!');
+		}
+		else{
+			Review.findOne({_id:review_id}, function(err, review) {
+		        if(err){
+		            res.send(err);
+		        }
+		        else{
+			        if(!review){
+		                res.send('Review with this id does not exist!');
+		            }
+		            else{
+			            if(review.user_id!=review_user_id){
+			            	res.send('You can only edit your own review!');
+			            }
+				        else{
+				        	review.comment = review_newComment;
+				            review.save(function(err) {
+				                if(err){
+				                    res.send(err);
+				                } 
+				                else {
+				                    res.send('Review has been updated successfully.');
+				                }
+				            });
+
+				        }
+		            }
+		        }
+	        });
+		}
+	},
+
+	//by using angular's ng-show, it will be guaranteed that each user can only delete his own reviews
+	//this function receives requests for deleting reviews
+	//the function ensures that the request includes the required field (user_id)
+	//by using angular's ng-show, it will be guaranteed that each user can only delete his own reviews
+	deleteReview: function(req,res){
+		var review_user_id = req.body.user_id;
+        var review_id =  req.params.id;
+        var missingFields = review_user_id==null || review_user_id=='';
+		if(missingFields){
+			res.send('The field: (user_id) is required!');
+		}
+		else{
+			Review.findOne({_id:review_id}, function(err, review){
+	            if(err){
+	                res.send(err);
+	            }
+	            else{
+		            if(!review){
+		                res.send('Review with this id does not exist!');
+		            }
+		            else{
+			            if(review.user_id!=review_user_id){
+			            	res.send('You can only delete your own review!');
+			            }
+			            else{
+			            	Review.findOneAndRemove({ _id:review_id }, function(err, reviewToBeDeleted) {
+		                        if(err){
+		                        	res.send(err);
+		                        }
+		                        else{
+		                        	res.send('Review has been deleted successfully.'); 
+		                        }
+	                    	});
+			                
+			            }
+		            }
+	            }
+       		});
+		}
+	}, 
+    
+  viewBusinessReviews: function (req, res) {
+
+  
+    var BusID = req.params.businessownerID;
+
+    var conditions = {
+        "business_id": BusID
+    };
+
+    var reviews = Review.find(conditions, function (err, reviews) {
+
+        if (err) {
+
+            res.send(err);
+
+           
+
+        } else {
+
+            res.send(reviews);
+
+        }
+
+    });
+  }  
+
+
+
+};
+
+module.exports = reviewController;
+
+
---
+++ app/routes.js
@@ -0,0 +1,156 @@
+//Dependencies
+var express = require('express');
+var router = express.Router();
+var reviewController=require('./controllers/reviewController');
+var activityController=require('./controllers/activityController');
+var administratorController = require('./controllers/administratorController');
+var applicationController = require('./controllers/applicationController');
+var businessOwnerController = require('./controllers/businessownerController');
+var passport=require('passport');
+var LocalStrategy=require('passport-local').Strategy;
+var clientController = require('./controllers/clientController');
+var userController = require('./controllers/userController');
+var authController = require('./controllers/AuthenticationController');
+
+
+//Passport authentication
+passport.use('login', new LocalStrategy(
+  //here we have username and password as an input parameters
+  //we search if a client exists with in the Client table so we authenticate the client
+  //If we couldn't find a client with matching username and password, so we search if
+  //the data passed from the front-end is matching admin credentials, thus, the
+  //authentication would be done for admins.
+  //If the data didn't match also the admin credentials, so we search in the BusinessOwner
+  //table, if we found a matched tuple then the authentication would be done for 
+  //BusinessOwner, if not found then the data entered doesn't exist in my system
+  //an error message is displayed accordingly.
+  function(username, password, done) {
+    
+   clientController.getClient(username,password, function(err, client){
+    if(err) {
+      return done(null, false, {message: 'Error Happened'});
+    }
+    if(client){
+      return done(null, client);
+   }
+   else{
+   administratorController.comparePassword(password,function(err, isAdmin){
+      if(err){
+        return done(null, false, {message: 'Error Happened'});
+      } 
+      if(isAdmin && username=="admin"){
+        administratorController.getAdmin(function(err,admin)
+        {
+          if(err)
+          {
+            return done(null, false, {message: 'Error Happened'});
+          }
+          return done(null, admin[0]);
+        });
+      }
+    else{
+    businessOwnerController.getOwner(username,password,function(err,businessOwner)
+    {
+      if(err)
+      {
+        return done(null, false, {message: 'Error Happened'});
+      }
+      if(businessOwner)
+      {
+        return done(null, businessOwner);
+      }
+      else{
+        return done(null, false, {message: 'Invalid username or password'});
+      }
+
+    });
+    }
+    });
+  }
+   });
+  }));
+
+passport.serializeUser(function(user, done) {
+  done(null,user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null,obj);
+});
+//Routes
+
+//routing for creating a new Cient
+router.post('/register',userController.createUser); //done --
+
+//routing for viewing the summary of venues of the Business owners
+router.get('/view-summary',clientController.viewSummaries);//done --
+
+//routing for updating client accont information(firstname,lastname,gender,email,phonenumber)
+router.post('/update-Info',clientController.updateInfo);//done --
+
+//routing for viewing detailed venue page for a specific Business Owner
+router.get('/businessOwner/:id', clientController.viewBusiness);//done --
+
+//routing for changing the username of the User
+router.post('/change-username',userController.changeUsername);//done --
+
+//
+router.get('/application/:username/reject',applicationController.reject);//done --
+router.get('/application/:username/accept',applicationController.accept);//done --
+router.post('/user/forgotPassword',userController.forgotPassword);//done --
+
+//routing for viewing applications
+router.get('/applications/:page', administratorController.viewApplicationsIndex);//done --
+router.get('/applications', administratorController.viewApplications);//done --
+
+//routing for creating application
+router.post('/business/apply', applicationController.createApplication);//done --
+
+//routing for updating basic info
+router.post('/business/update-info', businessOwnerController.updateInfo);//done
+
+//routing for locations operations
+router.post('/business/locations/add', businessOwnerController.addLocation);//done --
+router.post('/business/locations/remove', businessOwnerController.removeLocation);//done --
+
+//routing for security
+router.post('/security/change-password', businessOwnerController.changePassword);//done --
+
+router.get('/logout',authController.ensureAuthenticated, authController.generalLogOut);
+
+router.get('/search/:keyword',userController.search);//done--
+
+router.post('/gallery', businessOwnerController.addMedia);//done--
+router.post('/offer', businessOwnerController.addOffer);//done--
+router.get('/showReview/:businessownerID', businessOwnerController.showReview);//done--
+router.post('/reply/:reviewID', businessOwnerController.reply);//done--
+
+router.post('/review/newReview', reviewController.newReview);//done--
+router.put('/review/editReview/:id', reviewController.editReview);//done--
+router.delete('/review/deleteReview/:id', reviewController.deleteReview);//done--
+
+router.get('/review/view/:businessownerID', reviewController.viewBusinessReviews );//done--
+router.post('/business/rate', clientController.rateBusiness );//done--
+
+router.put('/activity/editActivity/:id', activityController.editActivity);//done--
+
+router.post('/addActivity/:BusinessOwnerId',businessOwnerController.addActivity);//done --
+router.get('/deleteActivity/:activityId/:BusinessOwnerId',businessOwnerController.deleteActivity);//done--
+
+router.get('/viewBusinesses',administratorController.viewBusinesses);//done--
+router.get('/removeBusiness/:businessId',administratorController.removeBusiness);//done--
+
+router.post('/createAdmin',administratorController.createAdmin);//done--
+
+router.post('/login',
+  passport.authenticate('login', {failureFlash: true}),
+  function(req, res) {
+    req.flash('success_msg', 'You are logged in correctly :)');
+    res.send("Logged In successfully");
+    return;
+  });//done--
+
+//export router
+module.exports = router;
+
+
