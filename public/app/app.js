1c26fde93f6b11f45130c7078a2725f19b567789
Ebraheem1 <ebraheem.mouhamed@gmail.com>
2017-04-17 22:46:34 +0200

    Search + login + logout are handeled front-end and back-end and also by ensuring authentication in logout

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,5 @@
 //the userApp is just a module, which contains as dependencies all other controllers
-angular.module('userApp', ['appRoutes', 'userControllers', 'userServices', 'ngAnimate', 'mainController', 'authServices', 'reviewController', 'reviewServices','businessOwnerServices', 'ui.bootstrap'])
+angular.module('userApp', ['appRoutes', 'userControllers', 'userServices', 'ngAnimate', 'mainController', 'authServices', 'reviewController', 'reviewServices','businessOwnerServices','pagingServices', 'ui.bootstrap'])
 
 
 .config(function($httpProvider){
