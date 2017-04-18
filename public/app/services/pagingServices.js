48f42928273234b226586e85b940a1ac33a7e69b
youssef <joex3999@gmail.com>
2017-04-18 13:39:05 +0200

    Add Pagination on Reservations

--- public/app/services/pagingServices.js
+++ public/app/services/pagingServices.js
@@ -1,11 +1,11 @@
 var app = angular.module('pagingServices', []);
 
-app.factory('Pager', function(){
+app.factory('Pager', function() {
     // service definition
     var pagerFactory = {};
 
     // service implementation
-    pagerFactory.getPager =  function(totalItems, currentPage, pageSize){
+    pagerFactory.getPager = function(totalItems, currentPage, pageSize) {
         // default to first page
         currentPage = currentPage || 1;
 
@@ -13,32 +13,32 @@ app.factory('Pager', function(){
         pageSize = pageSize || 10;
 
         // calculate total pages
-        var totalPages = Math.ceil(totalItems/pageSize);
+        var totalPages = Math.ceil(totalItems / pageSize);
 
         var startPage, endPage;
-        if(totalPages <= 10){
+        if (totalPages <= 10) {
 
             // less than 10 total pages so show all
             startPage = 1;
             endPage = totalPages;
-        }else{
+        } else {
             // more than 10 total pages so calculate start and end pages
-            if(currentPage <= 6){
+            if (currentPage <= 6) {
 
                 startPage = 1;
                 endPage = 10;
 
-            }else{
+            } else {
 
-                if(currentPage+4 >= totalPages){
+                if (currentPage + 4 >= totalPages) {
 
-                    startPage = totalPages-9;
+                    startPage = totalPages - 9;
                     endPage = totalPages;
 
-                }else{
+                } else {
 
-                    startPage = currentPage-5;
-                    endPage = currentPage+4;
+                    startPage = currentPage - 5;
+                    endPage = currentPage + 4;
 
                 }
 
@@ -47,20 +47,20 @@ app.factory('Pager', function(){
         }
 
         // calculate start and end item indexes
-        var startIndex = (currentPage-1)*pageSize;
-        var endIndex = Math.min(startIndex+pageSize-1, totalItems-1);
+        var startIndex = (currentPage - 1) * pageSize;
+        var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
 
         // create an array of pages to ng-repeat in the pager control
         var pages = []
         //_.range(startPage, endPage + 1);
-        for(var i = startPage; i<=endPage; i++){
+        for (var i = startPage; i <= endPage; i++) {
 
             pages.push(i);
 
         }
 
         // return object with all pager properties required by the view
-        return{
+        return {
 
             totalItems: totalItems,
             currentPage: currentPage,
