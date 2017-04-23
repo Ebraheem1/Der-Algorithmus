0dff95c6cdfc38faa260d750ce4a192e95d41b9e
Marwan <marwanadelfaresali@gmail.com>
2017-04-23 23:32:51 +0200

    documented

--- public/app/services/businessOwnerServices.js
+++ public/app/services/businessOwnerServices.js
@@ -11,19 +11,29 @@ angular.module('businessOwnerServices', [])
 		
 		return $http.get('/api/search/'+$routeParams.keyword);
 	};
+	
+	//This function calls the backend function that show  the reviews of the logged-in businessowner
+	//then it returns the results to the controller.
 	businessOwnerFactory.getReviews = function(id){
 		
 		return $http.get('/api/showReview/'+id);
 	};
+	
+	//This function calls the backend function that uploads an image in the gallery of the logged-in businessowner
+	
 
 	businessOwnerFactory.uploadMedia = function(image){
                     
 		return $http.post('/api/gallery',image);
 	};
+	
+	//This function calls the backend function that used to reply on a specific review by the logged-in businessowner
 	businessOwnerFactory.reply = function(replyData,reviewID){
 
 		return $http.post('/api/reply/'+reviewID,replyData);
 	};
+	
+	//This function calls the backend function that used for adding an offer on a specific activity.
 
     businessOwnerFactory.addOffer = function(offerData,activityID){
 
