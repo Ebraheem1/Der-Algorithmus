a2e47185623660e6e90fa660152f58cdfa31d369
Youssef Mohamed <joex3999@users.noreply.github.com>
2017-04-23 20:20:03 +0200

    Documentation

--- public/app/controllers/passwordController.js
+++ public/app/controllers/passwordController.js
@@ -1,10 +1,12 @@
 angular.module('passwordController', ['authServices','modalDialog'])
+  // This simple controller is used to manage the forgeting password function for the user . it takes the parameters from the view 
+  // and calls an http post request ('/api/user/forgotPassword') for checking the mail with the username  and sending a password accordingly to his mail 
+  // it then sends and error message or a success message accordingly
   .controller('passwordController',function(Authentication,$http, $scope, $location, $timeout, Reservation, $routeParams){
     var app = this ;
     $scope.done = false ;
 
     app.forgotPassword = function(params){
-      console.log(params);
       var response = $http.post('/api/user/forgotPassword',params).then(function(data){
         $scope.success = data.data.success;
         $scope.message = data.data.message;
