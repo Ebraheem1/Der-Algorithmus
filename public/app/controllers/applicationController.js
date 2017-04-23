adfb3c562fb6d5869b8179a62fa976d3aff6455a
ahmedElqutb <ahmedElqutb@users.noreply.github.com>
2017-04-23 23:54:01 +0200

    Update applicationController.js

--- public/app/controllers/applicationController.js
+++ public/app/controllers/applicationController.js
@@ -3,7 +3,9 @@
 	app.controller('applicationCtrl', function(Application, Authentication ,AuthenticationToken, $window, $location, $http, $scope, $routeParams){
 
 		var controller = this;
-
+		
+		//this function retrieves the info of application currently in review on component load
+		//to be viewed in detail by the admin.
 		Application.getApplication($routeParams.id).then(function(application){
 		
 			if(application.data.success){
@@ -26,7 +28,8 @@
 				}
 			
 			});
-
+		
+		//this function signals the associated service of acceptance of currently in-review application
 		this.accept = function(id){
 
 			Application.acceptApplication(id).then(function(application){
@@ -54,7 +57,8 @@
 			});
 
 		};
-
+		
+		//this function signals the associated service of a rejection of an application
 		this.reject = function(id){
 
 			Application.rejectApplication(id).then(function(application){
