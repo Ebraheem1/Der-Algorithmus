c57e371e71fcd7a0974fd9438818b21072d9c51b
Zakyyy <Ahmed.abdelghafour@student.guc.edu.eg>
2017-04-23 20:08:30 +0200

    Update clientServices.js

--- public/app/services/clientServices.js
+++ public/app/services/clientServices.js
@@ -4,13 +4,18 @@ angular.module('clientServices',[])
   clientFactory={};
 
 clientFactory.viewSummaries=function(){
+  // this service is responsible to view summaries of all business owners on the website, it just go to the back end and gets them all
   return $http.get('/api/view-summary');
 };
 
 clientFactory.viewDetailed=function(id){
+  //this service is responsible to view the detailed information of the business owner corresponding to the id passed as a parameter
+  // in the get request to the backend
   return $http.get('/api/businessOwner/'+id);
 }
 clientFactory.viewActivity=function(id){
+  //this service is responsible to view the detailed information of an activity corresponding to the id passed as a parameter
+  // in the get request to the backend
   return $http.get('/api/view-activity/'+id);
 }
   return clientFactory;
