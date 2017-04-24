e401bd690500cae4511fa8f0c6078b54ec4421d8
shorim <mostafa_shorim@hotmail.com>
2017-04-24 02:34:57 +0200

    Update nonRepeatableReservationsServices.js

--- public/app/services/nonRepeatableReservationsServices.js
+++ public/app/services/nonRepeatableReservationsServices.js
@@ -5,6 +5,8 @@ angular.module('nonRepeatableReservationsServices',[])
 
 	nonRepeatableReservationsFactory = {};
 
+	// Calls the appropriate backend function responsible for returning all the reservations of a non-repeatable 
+	// activity (Trip, Safari)
 	nonRepeatableReservationsFactory.get = function(activityId){
 		
 		return $http.get('/api/viewNonRepeatableReservations/'+activityId);
