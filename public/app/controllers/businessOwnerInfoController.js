c25c211154d71654a6eaed6e8e1f81c7b1c652f2
ahmedElqutb <ahmedElqutb@users.noreply.github.com>
2017-04-29 00:18:15 +0200

    Update businessOwnerInfoController.js

--- public/app/controllers/businessOwnerInfoController.js
+++ public/app/controllers/businessOwnerInfoController.js
@@ -12,7 +12,39 @@
 		$scope.editDescription =false;
 		$scope.emailAvailable = false;
 		$scope.manageLocations = false;
-		
+		$scope.isPhoneNumber = false;
+
+		$scope.tidyForm = function(){
+
+			if(!($scope.editName)){
+
+				controller.info.name = null;
+				$scope.data.name = null;
+
+			}
+
+			if(!($scope.editNumber)){
+
+				controller.info.phoneNumber = null;
+				$scope.data.phoneNumber = null;
+
+			}
+
+			if(!($scope.editEmail)){
+
+				controller.info.email = null;
+				$scope.data.email = null;
+
+			}
+
+			if(!($scope.editDescription)){
+
+				controller.info.description = null;
+				$scope.data.description = null;
+
+			}
+			
+		};
 		
 		//retrieve current information of business owner on page load
 		BusinessOwner.getInfo($routeParams.id).then(function(business){
@@ -35,6 +67,8 @@
 			}
 			
 			});
+
+
 		
 		//update information of business with supplied values.
 		this.updateInfo = function(){
@@ -104,17 +138,23 @@
 
       	};
 	//validation of email format
-      	this.isEmail = function(){
+      	$scope.isEmail = function(){
 
       		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     		return re.test(controller.info.email);
 
       	};
 	//matching new email against old one
-      	this.isOldEmail = function(){
+      	$scope.isOldEmail = function(){
 
     		return $scope.user.email == controller.info.email;
 
       	};
+
+      	$scope.isPhoneNumber = function(){
+
+      		$scope.isPhoneNumber = !isNaN(controller.info.phoneNumber)&&(controller.info.phoneNumber.length>4);
+
+    	};
 
 	});
